c6707a6e7a20bc43bd10cee55c0201501d33da71
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2020-05-07 01:29:54 +0200

    Add Program.cs with sample argument output

---
+++ pihole_search_blocklists/pihole_search_blocklists/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace pihole_search_blocklists
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var switchMappings = new Dictionary<string, string>()
+            {{ "-f", "filePath" }};
+            var builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args, switchMappings);
+
+            var config = builder.Build();
+            Console.WriteLine($"filepath: '{config["filePath"]}'");
+        }
+    }
+}
