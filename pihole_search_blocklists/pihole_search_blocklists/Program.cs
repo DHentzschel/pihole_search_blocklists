573ca2ad1e3a276dc5fcf62d30089b2e9f218881
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2020-05-07 02:36:26 +0200

    Add two more arguments -r regex and -o outputPath

--- pihole_search_blocklists/pihole_search_blocklists/Program.cs
+++ pihole_search_blocklists/pihole_search_blocklists/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace pihole_search_blocklists
 {
@@ -9,12 +11,20 @@ namespace pihole_search_blocklists
         static void Main(string[] args)
         {
             var switchMappings = new Dictionary<string, string>()
-            {{ "-f", "filePath" }};
+            {
+                { "-f", "filePath" },
+                { "-r", "regex" },
+                { "-o", "outputPath" }
+            };
+            if (args.Length < 3 || switchMappings.Any(p => !args.Contains(p.Key)))
+            {
+                Console.Error.WriteLine("Error: Please type in all required arguments: -f (filePath), -r (regex) and -o (outputPath)");
+                return;
+            }
             var builder = new ConfigurationBuilder();
             builder.AddCommandLine(args, switchMappings);
 
             var config = builder.Build();
-            Console.WriteLine($"filepath: '{config["filePath"]}'");
         }
     }
 }
